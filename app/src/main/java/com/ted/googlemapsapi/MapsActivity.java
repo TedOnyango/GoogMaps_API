6f673ed2d566f1be637922dd3e548fc5f34c1445
TedOnyango <teddonyango@gmail.com>
2019-09-01 22:46:04 +0300

    Improved map visuals

--- app/src/main/java/com/ted/googlemapsapi/MapsActivity.java
+++ app/src/main/java/com/ted/googlemapsapi/MapsActivity.java
@@ -26,22 +26,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng nairobi = new LatLng(1.2921, 36.8219);
+        mMap.addMarker(new MarkerOptions().position(nairobi).title("Marker in Nairobi"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(nairobi));
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.isBuildingsEnabled();
+
     }
 }
